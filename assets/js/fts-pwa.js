40b2e0773956c07e595c47a55b8eee78dcb7eb3d
chriskito30-a11y <chriskito30@gmail.com>
2026-05-19 00:42:04 +0200

    Add files via upload

--- assets/js/fts-pwa.js
+++ assets/js/fts-pwa.js
@@ -8,7 +8,7 @@
   if (!('serviceWorker' in navigator)) return;
 
   const SW_URL = './sw.js';
-  const RELOAD_KEY = 'fts-sw-reload-v45-member-news-sync';
+  const RELOAD_KEY = 'fts-sw-reload-v46-admin-center';
   let refreshing = false;
 
 
