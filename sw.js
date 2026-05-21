f8441d9824d6bfd279423070c1b7118146cb3508
chriskito30-a11y <chriskito30@gmail.com>
2026-05-21 12:49:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fts-v125-repetition-mobile-cards';
+const CACHE = 'fts-v126-repetition-mobile-cards';
 const FILES = [
   './manifest.json',
   './index.html',
@@ -194,7 +194,7 @@ self.addEventListener('message', function(event){
   }
 });
 
-const FTS_RUNTIME_CACHE = 'fts-v125-runtime';
+const FTS_RUNTIME_CACHE = 'fts-v126-runtime';
 const FTS_FILES_CACHE = 'fts-offline-files-v74';
 
 function isFirebaseOrAuthRequest(url){
